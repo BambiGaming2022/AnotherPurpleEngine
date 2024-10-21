6b4a9a58b81b11f2c303cb37a608f2b859bfb62f
BambiGaming2022 <windowsxpnever@gmail.com>
2024-10-21 20:45:42 +0300

    Initial commit

---
+++ source/WarningState.hx
@@ -0,0 +1,47 @@
+package;
+import flixel.*;
+import flixel.addons.transition.FlxTransitionSprite.GraphicTransTileDiamond;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.addons.transition.TransitionData;
+import flixel.util.FlxColor;
+import flixel.math.FlxPoint;
+
+/**
+ * ...
+ * sorry bbpanzu
+ */
+class WarningState extends MusicBeatState
+{
+
+	public function new() 
+	{
+		super();
+	}
+	override function create() 
+	{
+		super.create();
+		
+		var bg:FlxSprite = new FlxSprite();
+		
+		bg.loadGraphic(Paths.image("warning", "preload"));
+		add(bg);
+			
+	}
+	
+	
+	override function update(elapsed:Float) 
+	{
+		super.update(elapsed);
+		
+		
+		if (controls.ACCEPT){
+			FlxG.sound.play(Paths.sound('scrollMenu'));
+			MusicBeatState.switchState(new MainMenuState());
+		}
+		if (controls.BACK)
+		{
+			FlxG.sound.play(Paths.sound('cancelMenu'));
+			MusicBeatState.switchState(new TitleState());
+		}
+	}
+}
